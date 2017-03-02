569bb95a77df0e0ca728d0d259bd5cb3b52f288f
edersonlrf <edersonluis.rf@gmail.com>
2017-03-02 02:32:47 -0300

    a02 exercicio tres

---
+++ a02/exercicio3.cpp
@@ -0,0 +1,31 @@
+/*
+Ler um valor inteiro, ate que o usuario
+digite 471 ou erre tres vezes consecutivas.
+-> senha valida
+-> tres tentativas - erro
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+main(){
+    int senha, tentativa;
+
+    tentativa = 0;    
+    do {
+        printf("Digite a senha: ");
+        scanf("%i", &senha);
+        if (senha == 471) {
+            printf("\nSenha valida!\n\n");
+        } else {
+            tentativa++;
+        }
+        
+        if (tentativa == 3) {
+            printf("\nTres tentativas - erro!\n\n");
+            break;
+        }
+    } while(senha != 471);
+
+    system("pause");
+}
