b1dcc2b91d3acc3762da2ce8f2fb9c2d44d6da52
edersonlrf <edersonluis.rf@gmail.com>
2017-03-02 01:43:30 -0300

    a02 exercicio dois

---
+++ a02/exercicio2.cpp
@@ -0,0 +1,27 @@
+/*
+Ler 1 valor inteiro entre 1 e 10, escrever
+a tabuada do valor lido.
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+main(){
+    int v, i, tab;
+    
+    printf("Digite um valor entre 1 e 10: ");
+    scanf("%i", &v);
+
+    while(v < 1 || v > 10) {
+        printf("E r r o!\n");
+        printf("Digite um valor entre 1 e 10: ");
+        scanf("%i", &v);
+    }
+
+    for (i=1; i<=10; i++) {
+        tab = i * v;
+        printf("%.i x %.i = %.i\n", v, i, tab);
+    }
+
+    system("pause");
+}
