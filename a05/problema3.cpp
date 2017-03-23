2b0716b24d0a9ad9d3efda42470fde522fdcc9b9
edersonlrf <edersonluis.rf@gmail.com>
2017-03-23 20:39:13 -0300

    a05 resposta do problema 3

---
+++ a05/problema3.cpp
@@ -0,0 +1,18 @@
+/*
+Ler uma string com ate 40 caracteres,
+converter as letras minusculas em maiusculas
+e escrever a string modificada.
+*/
+
+#include<stdio.h>
+#include<conio.c>
+
+main(){
+    char frase[40];
+    
+    clrscr(); // Limpa a tela.
+    
+    
+    
+    getche(); // Da uma pausada.
+}
