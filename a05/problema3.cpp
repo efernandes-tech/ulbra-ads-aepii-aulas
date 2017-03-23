4fddb8658062d1cd563158a1dea3368f59a0eb0a
edersonlrf <edersonluis.rf@gmail.com>
2017-03-23 20:56:37 -0300

    a05 resposta do exercicio 3

--- a05/problema3.cpp
+++ a05/problema3.cpp
@@ -11,8 +11,11 @@ main(){
     char frase[40];
     
     clrscr(); // Limpa a tela.
-    
-    
-    
+    gotoxy(5,4);
+    printf("Digite uma frase: ");
+    fflush(stdin); // Limpa o buffer.
+    fgets(frase,40,stdin); // Pega a frase.
+    gotoxy(5,10);
+    printf(strupr(frase)); // Exibe a frase com as letras em maiusculo.
     getche(); // Da uma pausada.
 }
