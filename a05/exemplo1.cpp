6194876c9b682b65c07da4ccdd3ca60cddc90431
edersonlrf <edersonluis.rf@gmail.com>
2017-03-23 19:44:42 -0300

    livro 1.7 codigos de formato

---
+++ a05/exemplo1.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(void) {
+	float valor1 = 136.472;
+	printf("\n %f", valor1);
+	printf("\n %8.1f", valor1);
+	printf("\n %08.1f", valor1);
+	printf("\n %-8.1f", valor1);
+	
+	int valor2 = 790;
+	printf("\n %i", valor2);
+	printf("\n %5i", valor2);
+	printf("\n %05i", valor2);
+	printf("\n %-5i", valor2);
+	
+	printf("\n O total é %2i.", 350);
+	printf("\n O total é %4i.", 350);
+	printf("\n O total é %5i.", 350);
+
+	return 0;
+}
